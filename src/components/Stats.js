e7f3a217b2cfbe12935c536817c287add9dc9de7
Emir Jasarevic <61988938+emir3100@users.noreply.github.com>
2021-10-29 21:16:02 +0200

    added opensea get api

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -19,7 +19,7 @@ const styles = theme => ({
 function Stats(props) {
     const { classes } = props;
 
-    const url = 'https://api.opensea.io/api/v1/asset/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/1/';
+    const url = 'https://api.opensea.io/api/v1/collections?asset_owner=0x2a1ea3ac74703c4efbf09252d6e859296344de5e&offset=0';
     const [collection, setCollection] = useState(null);
 
     useEffect(()=> {
@@ -29,50 +29,98 @@ function Stats(props) {
         })
     }, [url])
     
-    return (
-        <div id="Stats" className="stats-wrapper" style={{"position": "relative"}}>
-                <div className="custom-container" style={{"paddingBlock" : "7rem"}} >
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} style={{"marginBottom" : "5rem"}}>
-                            <Typography align="start" variant="h3" gutterBottom>
-                                Stats
-                            </Typography>
+    if(collection){
+        return (
+            <div id="Stats" className="stats-wrapper" style={{"position": "relative"}}>
+                    <div className="custom-container" style={{"paddingBlock" : "7rem"}} >
+                        <Grid container spacing={3}>
+                            <Grid item xs={12} style={{"marginBottom" : "5rem"}}>
+                                <Typography align="start" variant="h3" gutterBottom>
+                                    Stats
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={3} >
+                                <Typography align="center" gutterBottom variant="subtitle1" component="div">
+                                    Items
+                                </Typography>
+                                <Typography align="center" variant="h6" gutterBottom>
+                                    {collection[0].stats.count}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={3}>
+                                <Typography align="center" gutterBottom variant="subtitle1" component="div">
+                                    Owner
+                                </Typography>
+                                <Typography align="center" variant="h6" gutterBottom>
+                                    {collection[0].stats.num_owners}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={3}>
+                                <Typography align="center" gutterBottom variant="subtitle1" component="div">
+                                    Floor price
+                                </Typography>
+                                <Typography align="center" variant="h6" gutterBottom>
+                                    {collection[0].stats.floor_price} eth
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={3}>
+                                <Typography align="center" gutterBottom variant="subtitle1" component="div">
+                                    Volume traded
+                                </Typography>
+                                <Typography align="center" variant="h6" gutterBottom>
+                                    {collection[0].stats.total_volume} eth
+                                </Typography>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={6} md={3} >
-                            <Typography align="center" gutterBottom variant="subtitle1" component="div">
-                                Items
-                            </Typography>
-                            <Typography align="center" variant="h6" gutterBottom>
-                                12
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={3}>
-                            <Typography align="center" gutterBottom variant="subtitle1" component="div">
-                                Owner
-                            </Typography>
-                            <Typography align="center" variant="h6" gutterBottom>
-                                1
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={3}>
-                            <Typography align="center" gutterBottom variant="subtitle1" component="div">
-                                Floor price
-                            </Typography>
-                            <Typography align="center" variant="h6" gutterBottom>
-                                0.1 eth
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={3}>
-                            <Typography align="center" gutterBottom variant="subtitle1" component="div">
-                                Volume traded
-                            </Typography>
-                            <Typography align="center" variant="h6" gutterBottom>
-                                0.00 eth
-                            </Typography>
-                        </Grid>
-                    </Grid>
+                    </div>
                 </div>
+        )
+    }
+
+    return(
+        <div id="Stats" className="stats-wrapper" style={{"position": "relative"}}>
+            <div className="custom-container" style={{"paddingBlock" : "7rem"}} >
+                <Grid container spacing={3}>
+                    <Grid item xs={12} style={{"marginBottom" : "5rem"}}>
+                        <Typography align="start" variant="h3" gutterBottom>
+                            Stats
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3} >
+                        <Typography align="center" gutterBottom variant="subtitle1" component="div">
+                            Items
+                        </Typography>
+                        <Typography align="center" variant="h6" gutterBottom>
+                            No data
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Typography align="center" gutterBottom variant="subtitle1" component="div">
+                            Owner
+                        </Typography>
+                        <Typography align="center" variant="h6" gutterBottom>
+                            No data
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Typography align="center" gutterBottom variant="subtitle1" component="div">
+                            Floor price
+                        </Typography>
+                        <Typography align="center" variant="h6" gutterBottom>
+                            No data
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Typography align="center" gutterBottom variant="subtitle1" component="div">
+                            Volume traded
+                        </Typography>
+                        <Typography align="center" variant="h6" gutterBottom>
+                            No data
+                        </Typography>
+                    </Grid>
+                </Grid>
             </div>
+        </div>
     )
 }
 
