37727caf6509851136070e52e28f413d27df2bee
Emir Jasarevic <61988938+emir3100@users.noreply.github.com>
2021-11-02 21:55:40 +0100

    removed bootstrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
 ReactDOM.render( 
